a162716128172747836019eb2fd86d0ebfdca9be
Shane <setherith@gmail.com>
2020-03-29 19:42:32 +0100

    Added grid concept...

--- src/experimental/Asteroid.java
+++ src/experimental/Asteroid.java
@@ -19,17 +19,31 @@ public class Asteroid extends JPanel {
         Random rand = new Random();
         int angle = 360 / grade; int bearing = 0;
         int[] xs = new int[grade]; int[] ys = new int[grade];
+        
+        int xmax = 0; int xmin = getWidth();
+        int ymax = 0; int ymin = getHeight();
 
         for (int i = 0; i < grade; i++) {
             xs[i] = (int) (x + Math.cos(Math.toRadians(bearing)) * size - (rand.nextDouble() * size * 0.25));
+            if (xs[i] > xmax) xmax = xs[i];
+            if (xs[i] < xmin) xmin = xs[i];
             ys[i] = (int) (y + Math.sin(Math.toRadians(bearing)) * size - (rand.nextDouble() * size * 0.25));
+            if (ys[i] > ymax) ymax = ys[i];
+            if (ys[i] < ymin) ymin = ys[i];
             bearing += angle;
         }
 
+        // draw grid
+    	g.drawLine(0, ymin, getWidth(), ymin); // bottom
+    	g.drawLine(xmin, 0, xmin, getHeight()); // right
+    	
+    	g.drawLine(0, ymax, getWidth(), ymax); // top
+    	g.drawLine(xmax, 0, xmax, getHeight()); // left
+    	
         g.drawPolyline(xs, ys, grade);
         g.drawLine(xs[0], ys[0], xs[xs.length - 1], ys[ys.length - 1]);
     }
-
+    
     public static void main(String[] args) {
         new Asteroid();
     }
